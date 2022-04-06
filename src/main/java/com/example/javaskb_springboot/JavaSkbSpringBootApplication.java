16972a6f91dd89d73962cdcaaec14f363f4d5a9c
LexKeivi <lex.keivi@mail.ru>
2022-04-06 14:01:08 +0500

    Initial commit

---
+++ src/main/java/com/example/javaskb_springboot/JavaSkbSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaskb_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkbSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSkbSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaskb_springboot/JavaSkbSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaskb_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSkbSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
